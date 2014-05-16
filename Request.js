6c9f18358360b442d9e4445b1dd693b6e867ebb8
danny <danny.hupeng@gmail.com>
2014-05-16 18:01:16 +0800

    init library

---
+++ Request.js
@@ -0,0 +1,19 @@
+(function(window, undefined){
+    window.Request = {};
+
+    Request.get = function(){
+
+    }
+
+    Request.post = function(){
+
+    }
+
+    Request.head = function(){
+
+    }
+
+    Request.ajax = function(){
+        
+    }
+})(window, undefined)
